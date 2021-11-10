35df37dcdad2e8504d3831a088ed0b95626abb6f
Eberhard Lauth <eberhard.lauth@netzkundig.com>
2021-11-10 16:52:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 window.addEventListener('load', function (event) {
-    // initialized
-    console.log('window loaded');
-    // check status 
+    // window loaded
+
     var uc_user_interaction = localStorage.getItem('uc_user_interaction');
-    console.log('interaction status: ' + uc_user_interaction);
     if (!uc_user_interaction) {
-        console.log('let’s show the button');
         var uc_button_container = document.getElementById('usercentrics-root');
         uc_button_container.style.display = 'block';
     } else {
-        console.log('let’s hide the button');
         var uc_button_container = document.getElementById('usercentrics-root');
         uc_button_container.style.display = 'none';
     }
 });
 window.addEventListener('UC_UI_VIEW_CHANGED', function (event) {
-    console.log('Current view is: ' + event.detail.view);
     if (event.detail.view == 'PRIVACY_BUTTON') {
         var uc_button_container = document.getElementById('usercentrics-root');
         uc_button_container.style.display = 'none';
